7ba3970ea5d39abd96870c0a9d23d4ed027e1021
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-06-08 21:33:22 +0500

    Add Ticket model class

---
+++ src/Ticket.java
@@ -0,0 +1,12 @@
+
+public class Ticket {
+    String title;
+    String times;
+    int fare;
+
+    public Ticket(String title, String times, int fare) {
+        this.title = title;
+        this.times = times;
+        this.fare = fare;
+    }
+}
